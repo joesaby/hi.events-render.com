a639267ae63e58e34b17bf0a06344a638d4a49df
Jose Sebastian <joesaby@users.noreply.github.com>
2025-04-22 19:53:17 +0100

    Update render-start.sh

--- render-start.sh
+++ render-start.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-if [ -z "$RENDER_EXTERNAL_URL" ]; then
+if [ -z "$GICC_EVENTS_EXTERNAL_URL" ]; then
     export VITE_API_URL_CLIENT="/api"
     export APP_CND_URL="/storage"
 else
-    export VITE_API_URL_CLIENT="${RENDER_EXTERNAL_URL}/api"
+    export VITE_API_URL_CLIENT="${GICC_EVENTS_EXTERNAL_URL}/api"
     export APP_CND_URL="${RENDER_EXTERNAL_URL}/storage"
 fi
 
 export VITE_API_URL_SERVER=http://localhost:80/api
-export VITE_FRONTEND_URL=${RENDER_EXTERNAL_URL:-"/"}
+export VITE_FRONTEND_URL=${GICC_EVENTS_EXTERNAL_URL:-"/"}
 export VITE_STRIPE_PUBLISHABLE_KEY=${VIRE_STRIPE_PUBLISHABLE_KEY:-"pk_test-change-me"}
 
 echo "Starting with the following environment variables:"
-echo "RENDER_EXTERNAL_URL=${RENDER_EXTERNAL_URL}"
+echo "GICC_EVENTS_EXTERNAL_URL=${GICC_EVENTS_EXTERNAL_URL}"
 echo "VITE_API_URL_CLIENT=${VITE_API_URL_CLIENT}"
 echo "VITE_API_URL_SERVER=${VITE_API_URL_SERVER}"
 echo "VITE_FRONTEND_URL=${VITE_FRONTEND_URL}"
